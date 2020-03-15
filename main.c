9fa9245b671c882e3162bc05b3d5d31c3c085364
Jason Tsorlinis <jasontsorlinis@192-168-1-105.tpgi.com.au>
2020-03-15 11:33:56 +1100

    Just need to implement strategy

--- main.c
+++ main.c
@@ -1,24 +1,50 @@
+#include <stdbool.h>
 #include <stdio.h>
 #include <time.h>
-#include <stdbool.h>
 
+#include "cardpile.h"
 #include "table.h"
 #include "vector.h"
+#include "player.h"
 
-int main(int argc, char const *argv[])
-{
+const int num_players = 5;
+const int num_decks = 8;
+const int bet_size = 10;
+const int min_cards = 40;
+
+int rounds = 1000;
+const bool verbose = false;
+
+int main(int argc, char const *argv[]) {
+  if (argc == 2) rounds = atoi(argv[1]);
+
+  Table *t = Table__new(5, 8, 10, 40, verbose);
+  CardPile__shuffle(t->m_card_pile);
 
   clock_t start = clock();
 
-  Table *t = Table__new(5, 8, 10, 40, true);
+  for (int x = 0; x < rounds; x++) {
+    if(verbose) printf("Round %d\n", x+1);
+    if(!verbose && rounds > 1000 && x % (rounds / 100) == 0) {
+      printf("\tProgress: %d%%\r", x*100/rounds);
+      fflush(stdout);
+    }
 
-  Table__start_round(t);
-  Table__split(t);
+    Table__start_round(t);
+    Table__check_earnings(t);
+  }
+
   Table__clear(t);
-  Table__print(t);
-  
 
-  printf("Took %f seconds", (double)(clock() - start) / CLOCKS_PER_SEC);
+  for(int i = 0; i < t->m_players->size; i++) {
+    printf("Player %s earnings: %f\t\tWin Percentage: %f%%\n", 
+      ((Player*) t->m_players->items[i])->m_player_num, 
+      ((Player*) t->m_players->items[i])->m_earnings, 
+      (50 + ((Player*) t->m_players->items[i])->m_earnings / (rounds * bet_size) * 50.0));
+  }
+
+  printf("Casino earnings: %f\n\n", t->m_casino_earnings);
+  printf("Played %d rounds in %f seconds\n", rounds, (double)(clock() - start) / CLOCKS_PER_SEC);
 
   return 0;
 }
--- table.c
+++ table.c
@@ -1,6 +1,6 @@
 #include "table.h"
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "card.h"
 #include "cardpile.h"
@@ -75,91 +75,236 @@ void Table__deal_dealer(Table* self, bool face_down) {
 }
 
 void Table__start_round(Table* self) {
-    Table__clear(self);
-    Table__update_count(self);
-    if(self->m_verbose) {
-        printf("%d cards left\n", self->m_card_pile->m_cards->size);
-        printf("Running count is: %d\tTrue count is: %d\n", self->m_running_count, self->m_true_count);
-    }
+  Table__clear(self);
+  Table__update_count(self);
+  if (self->m_verbose) {
+    printf("%d cards left\n", self->m_card_pile->m_cards->size);
+    printf("Running count is: %d\tTrue count is: %d\n", self->m_running_count,
+           self->m_true_count);
+  }
 
-    Table__get_new_cards(self);
-    Table__pre_deal(self);
-    Table__deal_round(self);
-    Table__deal_dealer(self, false);
-    Table__deal_round(self);
-    Table__deal_dealer(self, true);
-    Table__evaluate_all(self);
-    self->m_current_player = 0;
-    if(Table__check_dealer_natural(self)) {
-        Table__finish_round(self);
-    } else {
-        Table__check_player_natural(self);
-        if(self->m_verbose) Table__print(self);
-        Table__auto_play(self);
-    }
-
+  Table__get_new_cards(self);
+  Table__pre_deal(self);
+  Table__deal_round(self);
+  Table__deal_dealer(self, false);
+  Table__deal_round(self);
+  Table__deal_dealer(self, true);
+  Table__evaluate_all(self);
+  self->m_current_player = 0;
+  if (Table__check_dealer_natural(self)) {
+    Table__finish_round(self);
+  } else {
+    Table__check_player_natural(self);
+    if (self->m_verbose) Table__print(self);
+    Table__auto_play(self);
+  }
 }
 
 void Table__get_new_cards(Table* self) {
-    if(self->m_card_pile->m_cards->size >= self->m_min_cards) return;
-    CardPile__refresh(self->m_card_pile);
-    CardPile__shuffle(self->m_card_pile);
-    self->m_true_count = 0;
-    self->m_running_count = 0;
-    if(self->m_verbose) {
-        printf("Got %d new decks as number of cards left is below %d", self->m_num_decks, self->m_min_cards);
-    }
+  if (self->m_card_pile->m_cards->size >= self->m_min_cards) return;
+  CardPile__refresh(self->m_card_pile);
+  CardPile__shuffle(self->m_card_pile);
+  self->m_true_count = 0;
+  self->m_running_count = 0;
+  if (self->m_verbose) {
+    printf("Got %d new decks as number of cards left is below %d\n",
+           self->m_num_decks, self->m_min_cards);
+  }
 }
 
 void Table__clear(Table* self) {
-    for(int i = self->m_players->size - 1; i >= 0; i--) {
-        Player__reset_hand(self->m_players->items[i]);
-        if(((Player*) self->m_players->items[i])->m_split_from != NULL) {
-            Player__free(self->m_players->items[i]);
-            Vector__delete(self->m_players, i);
-        }
+  for (int i = self->m_players->size - 1; i >= 0; i--) {
+    Player__reset_hand(self->m_players->items[i]);
+    if (((Player*)self->m_players->items[i])->m_split_from != NULL) {
+      Player__free(self->m_players->items[i]);
+      Vector__delete(self->m_players, i);
     }
-    Dealer__reset_hand(self->m_dealer);
-    self->m_current_player = 0;
+  }
+  Dealer__reset_hand(self->m_dealer);
+  self->m_current_player = 0;
 }
 
 void Table__update_count(Table* self) {
-    if(self->m_card_pile->m_cards->size > 51) {
-        self->m_true_count = self->m_running_count / (self->m_card_pile->m_cards->size / 52);
-    }
+  if (self->m_card_pile->m_cards->size > 51) {
+    self->m_true_count =
+        self->m_running_count / (self->m_card_pile->m_cards->size / 52);
+  }
 }
 
 void Table__hit(Table* self) {
-    Table__deal(self);
-    Player__evaluate(self->m_players->items[self->m_current_player]);
-    if(self->m_verbose) printf("Player %s hits\n", ((Player*) self->m_players->items[self->m_current_player])->m_player_num);
+  Table__deal(self);
+  Player__evaluate(self->m_players->items[self->m_current_player]);
+  if (self->m_verbose)
+    printf("Player %s hits\n",
+           ((Player*)self->m_players->items[self->m_current_player])
+               ->m_player_num);
 }
 
-void Table__stand(Table* self){
-    if(self->m_verbose && ((Player*) self->m_players->items[self->m_current_player])->m_value <= 21) {
-        printf("Player %s stands\n", ((Player*) self->m_players->items[self->m_current_player])->m_player_num);
+void Table__stand(Table* self) {
+  if (self->m_verbose &&
+      ((Player*)self->m_players->items[self->m_current_player])->m_value <=
+          21) {
+    printf("Player %s stands\n",
+           ((Player*)self->m_players->items[self->m_current_player])
+               ->m_player_num);
+  }
+  ((Player*)self->m_players->items[self->m_current_player])->m_is_done = true;
+}
+
+void Table__split(Table* self) {
+  Player* split_player =
+      Player__new(self, self->m_players->items[self->m_current_player]);
+  Vector__insert(self->m_players, split_player, self->m_current_player + 1);
+  Player__evaluate(self->m_players->items[self->m_current_player]);
+  Player__evaluate(self->m_players->items[self->m_current_player + 1]);
+  if (self->m_verbose)
+    printf("Player %s splits\n",
+           ((Player*)self->m_players->items[self->m_current_player])
+               ->m_player_num);
+}
+
+void Table__split_aces(Table* self) {
+  if (self->m_verbose)
+    printf("Player %s splits aces\n",
+           ((Player*)self->m_players->items[self->m_current_player])
+               ->m_player_num);
+  Player* split_player =
+      Player__new(self, self->m_players->items[self->m_current_player]);
+  Vector__insert(self->m_players, split_player, self->m_current_player + 1);
+  Table__deal(self);
+  Player__evaluate(self->m_players->items[self->m_current_player]);
+  Table__stand(self);
+  self->m_current_player++;
+  Table__deal(self);
+  Player__evaluate(self->m_players->items[self->m_current_player]);
+  Table__stand(self);
+  if (self->m_verbose) Table__print(self);
+}
+
+void Table__double_bet(Table* self) {
+  if (((Player*)self->m_players->items[self->m_current_player])->m_bet_mult ==
+          1 &&
+      ((Player*)self->m_players->items[self->m_current_player])->m_hand->size ==
+          2) {
+    Player__double_bet(self->m_players->items[self->m_current_player]);
+    if (self->m_verbose)
+      printf("Player %s doubles\n",
+             ((Player*)self->m_players->items[self->m_current_player])
+                 ->m_player_num);
+    Table__hit(self);
+    Table__stand(self);
+  } else {
+    Table__hit(self);
+  }
+}
+
+void Table__auto_play(Table* self) {
+  // TODO: implement strategy
+  while (
+      ((Player*)self->m_players->items[self->m_current_player])->m_value < 17 &&
+      ((Player*)self->m_players->items[self->m_current_player])->m_hand->size <
+          5) {
+    Table__hit(self);
+  }
+  Table__next_player(self);
+}
+
+void Table__action(Table* self, char* action) {
+  // TODO
+}
+
+void Table__dealer_play(Table* self) {
+  bool all_busted = true;
+  for (int i = 0; i < self->m_players->size; i++) {
+    if (((Player*)self->m_players->items[i])->m_value < 22) {
+      all_busted = false;
+      break;
     }
-    ((Player*) self->m_players->items[self->m_current_player])->m_is_done = true;
+  }
+  ((Card*)self->m_dealer->m_hand->items[1])->m_face_down = false;
+  self->m_running_count += ((Card*)self->m_dealer->m_hand->items[1])->m_count;
+  Dealer__evaluate(self->m_dealer);
+  if (self->m_verbose) {
+    printf("Dealer's turn\n");
+    Table__print(self);
+  }
+
+  if (all_busted) {
+    if (self->m_verbose)
+      printf("Dealer automatically wins cause all players busted\n");
+    Table__finish_round(self);
+  } else {
+    while (self->m_dealer->m_value < 17 && self->m_dealer->m_hand->size < 5) {
+      Table__deal_dealer(self, false);
+      Dealer__evaluate(self->m_dealer);
+      if (self->m_verbose) {
+        printf("Dealer hits\n");
+        Table__print(self);
+      }
+    }
+
+    Table__finish_round(self);
+  }
 }
 
-void Table__split(Table* self){
-    Player* split_player = Player__new(self, self->m_players->items[self->m_current_player]);
-    Vector__insert(self->m_players, split_player, self->m_current_player+1);
-    Player__evaluate(self->m_players->items[self->m_current_player]);
-    Player__evaluate(self->m_players->items[self->m_current_player+1]);
-    if(self->m_verbose) printf("Player %s splits\n", ((Player*) self->m_players->items[self->m_current_player])->m_player_num);
+void Table__next_player(Table* self) {
+  if (++self->m_current_player < self->m_players->size) {
+    Table__auto_play(self);
+  } else {
+    Table__dealer_play(self);
+  }
 }
 
-void Table__split_aces(Table* self){}
-void Table__double_bet(Table* self){}
-void Table__auto_play(Table* self){}
-void Table__action(Table* self, char* action){}
-void Table__dealer_play(Table* self){}
-void Table__next_player(Table* self){}
-void Table__check_player_natural(Table* self){}
-bool Table__check_dealer_natural(Table* self){return false;}
-void Table__check_earnings(Table* self){}
-void Table__finish_round(Table* self){}
+void Table__check_player_natural(Table* self) {
+  for (int i = 0; i < self->m_players->size; i++) {
+    if (((Player*)self->m_players->items[i])->m_value == 21 &&
+        ((Player*)self->m_players->items[i])->m_hand->size == 2 &&
+        ((Player*)self->m_players->items[i])->m_split_from == NULL) {
+      ((Player*)self->m_players->items[i])->m_has_natural = true;
+    }
+  }
+}
+
+bool Table__check_dealer_natural(Table* self) {
+  Dealer__evaluate(self->m_dealer);
+  if (self->m_dealer->m_value != 21) return false;
+  ((Card*)self->m_dealer->m_hand->items[1])->m_face_down = false;
+  self->m_running_count += ((Card*)self->m_dealer->m_hand->items[1])->m_count;
+  if (self->m_verbose) {
+    Table__print(self);
+    printf("Dealer has a natural 21\n");
+  }
+  return true;
+}
+
+void Table__check_earnings(Table* self) {
+  float check = 0;
+  for (int i = 0; i < self->m_players->size; i++) {
+    check += ((Player*)self->m_players->items[i])->m_earnings;
+  }
+  if (check * -1 == self->m_casino_earnings) return;
+  printf("Earnings don't match\n");
+  exit(1);
+}
+
+void Table__finish_round(Table* self) {
+  if (self->m_verbose) printf("Scoring round\n");
+  for (int i = 0; i < self->m_players->size; i++) {
+    if (((Player*)self->m_players->items[i])->m_has_natural) {
+      Player__win(self->m_players->items[i], 1.5);
+    } else if (((Player*)self->m_players->items[i])->m_value > 21) {
+      Player__lose(self->m_players->items[i]);
+    } else if (self->m_dealer->m_value > 21 ||
+               ((Player*)self->m_players->items[i])->m_value >
+                   self->m_dealer->m_value) {
+      Player__win(self->m_players->items[i], 1);
+    } else if (((Player*) self->m_players->items[i])->m_value == self->m_dealer->m_value) {
+
+    } else {
+        Player__lose(self->m_players->items[i]);
+    }
+  }
+}
 
 void Table__print(Table* self) {
   for (int i = 0; i < self->m_players->size; i++) {
