26c8bb623fe4da14d007955dd5427dc5e36448b3
Jason Tsorlinis <jtsorlinis@gmail.com>
2021-04-07 19:44:26 +1000

    use pcg and nearly divisionless for shuffling

--- src/cardpile.c
+++ src/cardpile.c
@@ -1,27 +1,49 @@
 #include "cardpile.h"
-#include "vector.h"
-#include "card.h"
 
-#include <time.h>
 #include <stdio.h>
+#include <time.h>
+
+#include "card.h"
+#include "vector.h"
 
 char* ranks[] = {"A", "2", "3",  "4", "5", "6", "7",
-                     "8", "9", "10", "J", "Q", "K"};
+                 "8", "9", "10", "J", "Q", "K"};
 char* suits[] = {"Clubs", "Hearts", "Spades", "Diamonds"};
 
-unsigned int seed;
+uint64_t state;
 
-unsigned int xorshift() {
-  seed ^= seed << 13;
-  seed ^= seed >> 17;
-  seed ^= seed << 5;
-  return seed;
+// From https://www.pcg-random.org/download.html#minimal-c-implementation
+uint32_t pcg32() {
+  uint64_t oldstate = state;
+  // Advance internal state
+  state = oldstate * 6364136223846793005ULL + 1;
+  // Calculate output function (XSH RR), uses old state for max ILP
+  uint32_t xorshifted = ((oldstate >> 18u) ^ oldstate) >> 27u;
+  uint32_t rot = oldstate >> 59u;
+  return (xorshifted >> rot) | (xorshifted << ((-rot) & 31));
+}
+
+// use nearly divisionless technique found here
+// https://github.com/lemire/FastShuffleExperiments
+uint32_t pcg32_range(uint32_t s) {
+  uint32_t x = pcg32();
+  uint64_t m = (uint64_t)x * (uint64_t)s;
+  uint32_t l = (uint32_t)m;
+  if (l < s) {
+    uint32_t t = -s % s;
+    while (l < t) {
+      x = pcg32();
+      m = (uint64_t)x * (uint64_t)s;
+      l = (uint32_t)m;
+    }
+  }
+  return m >> 32;
 }
 
 CardPile* CardPile__new(int numdecks) {
-  seed = time(NULL);
+  state = time(NULL);
   CardPile* cp = malloc(sizeof(CardPile));
-  cp->m_cards = Vector__new(52*numdecks);
+  cp->m_cards = Vector__new(52 * numdecks);
   for (int i = 0; i < numdecks; i++) {
     for (int suit = 0; suit < 4; suit++) {
       for (int rank = 0; rank < 13; rank++) {
@@ -34,20 +56,20 @@ CardPile* CardPile__new(int numdecks) {
 }
 
 void CardPile__refresh(CardPile* self) {
-    Vector__free(self->m_cards);
-    self->m_cards = Vector__copy(self->m_original_cards);
+  Vector__free(self->m_cards);
+  self->m_cards = Vector__copy(self->m_original_cards);
 }
 
 void CardPile__print(CardPile* self) {
   for (int i = 0; i < self->m_cards->size; i++) {
-    printf("%s", ((Card*) self->m_cards->items[i])->m_rank);
+    printf("%s", ((Card*)self->m_cards->items[i])->m_rank);
   }
   printf("\n");
 }
 
 void CardPile__shuffle(CardPile* self) {
   for (int i = self->m_cards->size - 1; i > 0; i--) {
-    int j = xorshift() % (i + 1);
+    int j = pcg32_range(i + 1);
     Card* temp = self->m_cards->items[i];
     self->m_cards->items[i] = self->m_cards->items[j];
     self->m_cards->items[j] = temp;
